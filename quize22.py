45ce4150259a9da46fce008ebfd22a96e42fb116
Alexandra <75161692+Sora-ar@users.noreply.github.com>
2024-03-26 16:17:24 +0200

    Delete quize22.py

--- quize22.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-import requests
-
-url = "https://newsdata.io/api/1/news?apikey=pub_40345eaba0334c93c96d1236e5c53e73a2212&q=coronavirus"
-
-response = requests.get(url)
-
-pprint(response.json())
