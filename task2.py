14add859fa7031a6708327e1879fcc3fd116d210
Alexandra <atamanenkoa@school73kharkov.com>
2024-03-30 18:22:50 +0200

    changes in task 2. tasks 1-4 completed. corrected past mistakes

--- task2.py
+++ task2.py
@@ -2,15 +2,15 @@ import argparse
 import logging
 import csv
 import requests
+import os.path
 
 
-# 1. Set up new file logger and don’t forget to log information about each step
-def get_log():
+def get_log(log_level):
     logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
+    logger.setLevel(log_level)
 
     file_handler = logging.FileHandler('app.log')
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(log_level)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
@@ -19,31 +19,65 @@ def get_log():
     return logger
 
 
-# 2. Explore the data provider site and download a csv file with 5k user accounts using your script.
-def get_data_into_csv(logger):
-    logger.info("Starting data retrieval and CSV writing process")
-
-    url = "https://randomuser.me/api/?results=5000"
+def get_user_data(url, destination_file):
     response = requests.get(url)
-    result = response.json()['results']
+    if response.status_code == 200:
+        result = response.json()['results']
+        with open(destination_file, 'w', newline='', encoding='utf-8') as file:
+            csv_writer = csv.DictWriter(file, fieldnames=result[0].keys())
+            csv_writer.writeheader()
+            csv_writer.writerows(result)
+    else:
+        return None
 
-    with open('user_data.csv', 'w', newline='', encoding='utf-8') as file:
-        csv_writer = csv.DictWriter(file, fieldnames=list(result[0].keys()))
+
+def filter_data(source_file, destination_file, filter_by, filter_value):
+    filtered_rows = []
+
+    with open(source_file, 'r', newline='', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if filter_by == 'gender' and row['gender'] == filter_value:
+                filtered_rows.append(row)
+            elif filter_by == 'number' and csv_reader.line_num <= int(filter_value) + 1:
+                filtered_rows.append(row)
+
+    with open(destination_file, 'w', newline='', encoding='utf-8') as file:
+        csv_writer = csv.DictWriter(file, fieldnames=filtered_rows[0].keys())
         csv_writer.writeheader()
-        csv_writer.writerows(result)
-
-    logger.info("Data retrieval and CSV writing process completed")
-
-    # with open('user_data.csv', 'r', encoding='utf-8') as file:
-    #     csv_reader = csv.DictReader(file)
-    #     for row in csv_reader:
-    #         print(row)
+        csv_writer.writerows(filtered_rows)
 
 
 def main():
-    logger = get_log()
-    get_data_into_csv(logger)
+    parser = argparse.ArgumentParser(
+        usage='task2.py [-h] [--destination_folder path --file_name name --filter_by selected_filter --filter_value value --log_level selected_level]')
+    parser.add_argument('--destination_folder', metavar='DESTINATION_FOLDER',
+                        help="Path to a folder where output file is going to be placed", required=True)
+    parser.add_argument('--file_name', metavar='FILE', default='output',
+                        help="Filename for the output CSV file", required=True)
+    parser.add_argument('--filter_by', metavar='FILTER', choices=['gender', 'number'],
+                        help='Filter data by gender or number of rows')
+    parser.add_argument('--filter_value', metavar='VALUE', help='Value to filter data by')
+    parser.add_argument('--log_level', metavar='LOG', nargs='?', default='INFO',
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Log level')
+
+    args = parser.parse_args()
+
+    logger = get_log(args.log_level)
+    logger.info("Starting data retrieval and CSV writing process")
+
+    url = "https://randomuser.me/api/?results=5000"
+    destination_file = os.path.join(args.destination_folder, f"{args.file_name}.csv")
+    get_user_data(url, destination_file)
+
+    if args.filter_by and args.filter_value:
+        filter_data(destination_file, destination_file, args.filter_by, args.filter_value)
+        logger.info(f"Filtered data based on {args.filter_by} = {args.filter_value}")
+
+    logger.info("Data retrieval and CSV writing process completed")
 
 
+# Script that can be run from command line:
+# python task2.py --destination_folder C:\Users\Admin\Desktop\University\2_year\2st_semester\MultiparadigmProgrammingLanguages\homeworks --file_name filtered_data --filter_by gender --filter_value male --log_level DEBUG
 if __name__ == "__main__":
     main()
