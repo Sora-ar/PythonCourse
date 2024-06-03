7a8301de08c00458d19ad26c1c88bbfa3f6109c6
Alexandra <atamanenkoa@school73kharkov.com>
2024-06-03 20:23:10 +0300

    added dockstrings, linter, completely fulfilled TK from Task 4 and 5.

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
+"""
+Main module for data processing.
+
+This module contains functions for adding data from a CSV file to a database,
+as well as performing various transactions and analyses on the data.
+"""
 import csv
 
 from constants import logger
-from db_connection import conn_db
-from transaction import get_exchange_rate, transfer_money
+from transaction import (transfer_money, add_discounts, bank_with_most_unique_senders, get_user_transactions,
+                         find_bank_of_oldest_client, find_bank_with_largest_capital, get_users_with_debts)
 from work_with_db import add_user_into_db, add_account_into_db, add_bank_into_db, delete_data_fr_db
 
 
-@conn_db
-def get_table_names(cursor):
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    table_names = cursor.fetchall()
-    print(table_names)
-    return [name[0] for name in table_names]
-
-
-def add_user(users, table_name):
+def add_user(users):
     """
     Adds user information to the database.
 
     :param users: Variable number of dictionaries containing user information.
-    :return: None
+    :return: None.
     """
-    # add_data_into_db(table_name[2], users)
     add_user_into_db(users)
-    logger.info(f'User information add successfully.')
+    logger.info('User information add successfully.')
 
 
 def add_bank(banks):
@@ -31,10 +28,10 @@ def add_bank(banks):
     Adds bank information to the database.
 
     :param banks: Variable number of dictionaries containing bank information.
-    :return: None
+    :return: None.
     """
     add_bank_into_db(banks)
-    logger.info(f'Bank information add successfully.')
+    logger.info('Bank information add successfully.')
 
 
 def add_account(accounts):
@@ -42,26 +39,24 @@ def add_account(accounts):
     Adds account information to the database.
 
     :param accounts: Variable number of dictionaries containing account information.
-    :return: None
+    :return: None.
     """
     add_account_into_db(accounts)
-    logger.info(f'Account information add successfully.')
+    logger.info('Account information add successfully.')
 
 
-def add_data_from_csv(file_path, table_name):
+def add_data_from_csv(file_path):
     """
     Adds data from a CSV file to the database.
 
     :param file_path: The path to the CSV file containing the data to be inserted.
-    :param table_name: The name of the table into which the data should be inserted.
-                       This parameter is used for logging and validation purposes.
-    :return:
+    :return: None.
     """
     with open(file_path, 'r', newline='', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             if 'user_full_name' in row:
-                add_user(row, table_name)
+                add_user(row)
             if 'bank_name' in row:
                 add_bank(row)
             if 'account_type' in row:
@@ -71,16 +66,31 @@ def add_data_from_csv(file_path, table_name):
 
 
 def main():
+    """
+    Executes a series of operations:
+        - Deletes all data from the database.
+        - Adds data from a CSV file into the database.
+        - Performs a money transfer between accounts.
+        - Adds random discounts to a subset of users.
+        - Retrieves users with debts.
+        - Finds the bank with the largest capital.
+        - Finds the bank serving the oldest client.
+        - Determines the bank with the unique senders of transactions.
+        - Retrieves transactions for a specific user over the last three months.
+
+    :return: None
+    """
     delete_data_fr_db()
     file_path = 'data.csv'
-    table_names = get_table_names()
-    print(table_names[2])
-    add_data_from_csv(file_path, table_names)
-    hj = get_exchange_rate()
-    print(hj)
-    sender = 2
-    receiver = 4
-    transfer_money(sender, receiver)
+    # table_names = get_table_names()
+    add_data_from_csv(file_path)
+    transfer_money(10, 7, 4000)
+    add_discounts()
+    get_users_with_debts()
+    find_bank_with_largest_capital()
+    find_bank_of_oldest_client()
+    bank_with_most_unique_senders()
+    get_user_transactions(10)
 
 
 if __name__ == "__main__":
